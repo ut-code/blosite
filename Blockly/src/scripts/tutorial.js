c8a27aeca5699e5cd9f4996dfb4c8b5b786e1a9c
nakomochi <168958256+nakomochi@users.noreply.github.com>
2024-10-28 19:53:46 +0900

    トップページとチュートリアルページのUI調整

--- Blockly/src/scripts/tutorial.js
+++ Blockly/src/scripts/tutorial.js
@@ -1,1 +1,7 @@
 import '/src/styles/tutorial.css';
+
+// ページ読み込み時のレイアウト崩れを防ぐための処理
+window.addEventListener('load', function() {
+    const content = document.getElementById('content');
+    content.style.visibility = 'visible';
+  });
