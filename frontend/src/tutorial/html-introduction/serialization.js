56079928495bf3ed588face33fcf97f83fcca01b
nakomochi <168958256+nakomochi@users.noreply.github.com>
2024-11-10 18:45:41 +0900

    データベースの反映　ディレクトリ名の変更

--- frontend/src/tutorial/html-introduction/serialization.js
+++ frontend/src/tutorial/html-introduction/serialization.js
@@ -9,6 +9,31 @@ import * as Blockly from 'blockly/core';
 // キャッシュの保存先を決定
 const storageKey = 'tutorialHTMLIntroductionWorkspace';
 
+// 初期状態のブロックをXMLで定義
+const xml = `
+<xml xmlns="http://www.w3.org/1999/xhtml">
+  <block type="html_html-head-body" x="20" y="20">
+    <statement name="HEAD">
+      <block type="html_title">
+        <field name="CONTENT">Sample</field>
+      </block>
+    </statement>
+    <statement name="BODY">
+      <block type="html_hn">
+        <statement name="CONTENT">
+              <block type="html_text">
+                <field name="TEXT">Hello World!</field>
+              </block>
+        </statement>
+      </block>
+    </statement>
+  </block>
+</xml>
+`;
+
+// XMLを読み込んでブロックを配置
+const xmlDom = Blockly.utils.xml.textToDom(xml);
+
 /**
  * Saves the state of the workspace to browser's local storage.
  * @param {Blockly.Workspace} workspace Blockly workspace to save.
@@ -24,10 +49,18 @@ export const save = function (workspace) {
  */
 export const load = function (workspace) {
   const data = window.localStorage?.getItem(storageKey);
-  if (!data) return;
+  if (!data) {
+    Blockly.Xml.domToWorkspace(xmlDom, workspace);
+    return;
+  };
 
   // Don't emit events during loading.
   Blockly.Events.disable();
   Blockly.serialization.workspaces.load(JSON.parse(data), workspace, false);
+
+  if(workspace.getAllBlocks().length === 0) {
+    Blockly.Xml.domToWorkspace(xmlDom, workspace);
+  }
+
   Blockly.Events.enable();
 };
