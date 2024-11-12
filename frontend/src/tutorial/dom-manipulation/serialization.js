5cbadd1e05c4243f2d6913fec28a245f195a1f42
nakomochi <168958256+nakomochi@users.noreply.github.com>
2024-11-12 10:58:46 +0900

    DOM操作のチュートリアルを追加

--- frontend/src/tutorial/dom-manipulation/serialization.js
+++ frontend/src/tutorial/dom-manipulation/serialization.js
@@ -9,6 +9,41 @@ import * as Blockly from 'blockly/core';
 // キャッシュの保存先を決定
 const storageKey = 'tutorialDOMManipulationWorkspace';
 
+// 初期状態のブロックをXMLで定義
+const xml = `
+<xml xmlns="http://www.w3.org/1999/xhtml">
+  <block type="html_html-head-body" x="20" y="20">
+    <statement name="HEAD">
+      <block type="html_title">
+        <field name="CONTENT">Sample</field>
+      </block>
+    </statement>
+    <statement name="BODY">
+      <block type="html_ul">
+        <statement name="CONTENT">
+          <block type="html_li">
+            <field name="CONTENT">Item 1</field>
+            <next>
+              <block type="html_li">
+                <field name="CONTENT">Item 2</field>
+                <next>
+                  <block type="html_li">
+                    <field name="CONTENT">Item 3</field>
+                  </block>
+                </next>
+              </block>
+            </next>
+          </block>
+        </statement>
+      </block>
+    </statement>
+  </block>
+</xml>
+`;
+
+// XMLを読み込んでブロックを配置
+const xmlDom = Blockly.utils.xml.textToDom(xml);
+
 /**
  * Saves the state of the workspace to browser's local storage.
  * @param {Blockly.Workspace} workspace Blockly workspace to save.
@@ -24,10 +59,19 @@ export const save = function (workspace) {
  */
 export const load = function (workspace) {
   const data = window.localStorage?.getItem(storageKey);
-  if (!data) return;
+  if (!data) {
+    Blockly.Xml.domToWorkspace(xmlDom, workspace);
+    return;
+  };
 
   // Don't emit events during loading.
   Blockly.Events.disable();
   Blockly.serialization.workspaces.load(JSON.parse(data), workspace, false);
+
+  if(workspace.getAllBlocks().length === 0) {
+    Blockly.Xml.domToWorkspace(xmlDom, workspace);
+  }
+
   Blockly.Events.enable();
 };
+
